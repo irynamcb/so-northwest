7b378f2b628ffa0d29c5472dcd177f1817e4950e
irynamcb <iryna.mcbride@gmail.com>
2020-04-21 18:44:46 -0700

    Add users controller

---
+++ app/controllers/api/users_controller.rb
@@ -0,0 +1,57 @@
+class Api::UsersController < ApplicationController
+
+def show
+
+    @user = User
+                .find(params[:id])
+    if @user
+        render :show
+    else 
+      flash.now[:errors] = ['Cant find this user']
+      render json: ['Cant find this user'], status: :not_found
+    end
+
+end
+
+def create
+    @user = User.new(user_params)
+    if @user.save
+        login!(@user) 
+        render :show
+    else  
+        flash.now[:errors] = @user.errors.full_messages
+        render json: @user.errors.full_messages, status: 422
+    end
+end
+
+def update
+
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      render :show
+    else
+      render json: @user.errors.full_messages, status: 422
+    end
+
+end
+
+def destroy
+
+    @user = User.find(params[:id])
+
+    if @user
+      @user.destroy
+      render :show
+    else
+      flash.now[:errors] = ['Cant find this user']
+      render json: ['Cant find this user'], status: 422
+    end
+
+end
+
+def user_params
+    params.require(:user).permit(:first_name, :email, :password, :city)
+end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,11 @@ Rails.application.routes.draw do
 
 root to: 'static_pages#root'
 
+namespace :api, defaults: {format: :json} do 
+  resources :users, only: [:create, :edit, :update, :delete, :show] 
+  resource :session, only: [:create, :destroy]
+  resources :search, only: [:index] 
+
+end
+
 end
