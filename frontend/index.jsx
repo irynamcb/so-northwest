3945a70812922ccefa1d816a17e3fd76e6cbf6da
irynamcb <iryna.mcbride@gmail.com>
2020-04-21 19:16:33 -0700

    Add app.jsx and root.jsx

--- frontend/index.jsx
+++ frontend/index.jsx
@@ -1,7 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import configureStore from './store/store';
+import Root from './components/root';
 
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener('DOMContentLoaded', () => {
+    let store;
+    if (window.currentUser) {
+        const preloadedState = {
+            entities: {
+                users: { [window.currentUser.id]: window.currentUser }
+            },
+            session: { id: window.currentUser.id }
+        };
+        store = configureStore(preloadedState);
+        delete window.currentUser;
+    } else {
+        store = configureStore();
+    }
+
+    if (process.env.NODE_ENV !== "production") {
+        window.getState = store.getState;
+        window.dispatch = store.dispatch;
+        window.createLike = createLike;
+        window.deleteLike = deleteLike;
+    }
+
+
     const root = document.getElementById("root");
-    ReactDOM.render(<h1>Welcome to So_Northwest</h1>, root);
+    ReactDOM.render(<Root store={store} />, root)
+
 });
