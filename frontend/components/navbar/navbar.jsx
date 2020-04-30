1572b1bd8db2c0b63dad1da4df6f42c43d85857c
irynamcb <iryna.mcbride@gmail.com>
2020-04-30 15:49:48 -0700

    Fix minor CSS

--- frontend/components/navbar/navbar.jsx
+++ frontend/components/navbar/navbar.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faTree, faSignInAlt} from '@fortawesome/free-solid-svg-icons'
+import { faTree, faSignInAlt, faCartPlus} from '@fortawesome/free-solid-svg-icons'
 
 
 export default class Navbar extends React.Component {
@@ -29,6 +29,7 @@ export default class Navbar extends React.Component {
 
     personalGreeting() {
         const logo = <FontAwesomeIcon icon={faTree} size="1x" pull="left" inverse transform="shrink-4" />
+        const cart = <FontAwesomeIcon icon={faCartPlus} size="1x" pull="left" inverse transform="shrink-4" />
         return (
        
             <div className="navbar">
@@ -36,7 +37,7 @@ export default class Navbar extends React.Component {
                     <Link to="/" className="home">{logo}So_Northwest</Link>
                     <div className="nav-content">
                         <div className="header-name"><Link to='/'>Hi, {this.props.currentUser.firstName} {this.props.currentUser.lastName}!</Link></div>
-                        <div><Link to="/shoppingCart" className="home">Cart</Link></div>
+                        <div><Link to="/shoppingCart" className="home">Cart{cart}</Link></div>
                         <div className="nav">
                             <button className="header-button" onClick={this.props.logout}>Log Out</button>
                         </div>
