cbbbc25f4f2406f99d79b75d655f074e9998f8fd
irynamcb <iryna.mcbride@gmail.com>
2020-04-30 23:26:02 -0700

    Style single item price and subtotal

--- frontend/components/cart/cart_item.js
+++ frontend/components/cart/cart_item.js
@@ -39,7 +39,7 @@ function CartItem(props) {
             <div className="cart-details">
                 <Counter val={itemCount} callback={onCountChange}/>
                 <h5>${item.price.toFixed(2)}</h5>
-                <h6>${(count * item.price).toFixed(2)}</h6>
+                <h5>${(count * item.price).toFixed(2)}</h5>
             </div>
         </div>
     )
