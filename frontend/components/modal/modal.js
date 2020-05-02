bfa5a4d84028a8d6392a5b56fd0fadfb1c4f1c1b
irynamcb <iryna.mcbride@gmail.com>
2020-05-02 00:27:14 -0700

    Add closeModal

--- frontend/components/modal/modal.js
+++ frontend/components/modal/modal.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import {useSelector, useDispatch} from 'react-redux';
 import { closeModal } from './modal_slice';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheckCircle, faTimesCircle } from '@fortawesome/free-solid-svg-icons';
 
 
 function Modal() {
 
     let dispatch = useDispatch;
+    const icon = <FontAwesomeIcon icon={faCheckCircle} size="1x" pull="left" inverse transform="shrink-4" style={{color: 'green'}}/>
+    const close = <FontAwesomeIcon icon={faTimesCircle} size="3x" pull="right" inverse transform="shrink-4" style={{ color: 'green' }}/>
     
     const { modal } = useSelector(state => {
 
@@ -29,8 +33,10 @@ function Modal() {
 
     return (
         <div className="modal-background" >
+            {close}
             <div className="modal-child" onClick={e => e.stopPropagation()}>
-                <h1>{text}</h1>
+                <h1>{icon} {text}</h1>
+                
             </div>
         </div>
     );
