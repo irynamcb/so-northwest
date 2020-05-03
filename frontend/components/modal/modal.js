b6b12090431e7481465278ca998b5eda7d35c542
irynamcb <iryna.mcbride@gmail.com>
2020-05-02 17:11:02 -0700

    Add Outside Click feature

--- frontend/components/modal/modal.js
+++ frontend/components/modal/modal.js
@@ -1,20 +1,27 @@
-import React from 'react';
-import {useSelector, useDispatch} from 'react-redux';
+import React, { useRef } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { closeModal } from './modal_slice';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCheckCircle, faTimesCircle } from '@fortawesome/free-solid-svg-icons';
+import useOutsideClick from '../counter/outside_click';
 
 
 function Modal() {
 
+    let ref = useRef();
     let dispatch = useDispatch();
     const icon = <FontAwesomeIcon icon={faCheckCircle} size="1x" pull="left" inverse transform="shrink-4" style={{color: 'green'}}/>
     const close = <FontAwesomeIcon icon={faTimesCircle} size="2x" pull="right" inverse transform="shrink-4" style={{ color: 'green' }}/>
     
     const { modal } = useSelector(state => {
-
         return {
             modal: state.modal.modal
+        }
+    });
+    
+    useOutsideClick(ref, () => {
+        if (modal) {
+        dispatch(closeModal());
         }
     });
 
@@ -32,7 +39,7 @@ function Modal() {
     }
 
     return (
-        <div className="modal-background" >
+        <div className="modal-background" ref={ref}>
             <div onClick={() => dispatch(closeModal())}>{close}</div>
             <div className="modal-child" onClick={e => e.stopPropagation()}>
                 <h1>{icon} {text}</h1>
