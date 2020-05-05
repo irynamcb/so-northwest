cbccb5359913030a634c3a8617cc369517518090
irynamcb <iryna.mcbride@gmail.com>
2020-05-04 22:58:16 -0700

    Fix index route for cart accounting for current_user

--- app/controllers/api/carts_controller.rb
+++ app/controllers/api/carts_controller.rb
@@ -2,13 +2,13 @@ class Api::CartsController < ApplicationController
 
 def index
 
-    @cart = Cart.all
+    @cart = Cart.where("user_id = ?", current_user.id)
 
     if @cart
-    render :index
+        render :index
     else
-    flash.now[:errors] = ['Empty cart']
-    render json: ['Empty cart'], status: :not_found
+        flash.now[:errors] = ['Empty cart']
+        render json: ['Empty cart'], status: :not_found
     end  
 
 end
