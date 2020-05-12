d98d1838acb0354973afc393e2846b3e0af319c0
irynamcb <iryna.mcbride@gmail.com>
2020-05-11 17:14:01 -0700

    Add ajax calls to cart api util file

--- frontend/util/cart_api_util.js
+++ frontend/util/cart_api_util.js
@@ -4,3 +4,20 @@ export const fetchCartItems = () => (
         method: 'GET'
     })
 );
+
+
+export const addCartItem = (cartItem) => (
+    $.ajax({
+        url: `/api/carts`,
+        method: 'POST',
+        data: { cartItem }
+    })
+);
+
+
+export const deleteCartItem = (cartItemId) => (
+    $.ajax({
+        url: `/api/carts/${cartItemId}`,
+        method: 'DELETE'
+    })
+);
