87b9afd34edc5f8a57b24a256511012ccadff148
irynamcb <iryna.mcbride@gmail.com>
2020-05-25 15:44:54 -0700

    Add Page500

---
+++ frontend/components/errors/page500.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Page500 = () => (
+    <div>
+        <h1>Something went wrong!/</h1>
+        <h1>The engineers have been notified!</h1>
+    </div>
+)
+
+export default Page500;
